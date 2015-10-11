8437701a9491efb22f929cbcb20e2dbbdbc4a914
Sergii050588 <mozgoved88@mail.ru>
2015-10-11 13:27:01 +0300

    ...

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -5,15 +5,24 @@ import java.io.InputStreamReader;
 public class HumanPlayer extends DefaultPlayer
 {
     @Override
-    public boolean makeAmove() throws IOException
+    public Byte makeAmove() throws IOException
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Make your move. || Choose from 1 to 3 on X and Y coordinates.");
-        String myMove = br.readLine();
-        Byte myBestMove = Byte.parseByte(myMove);
 
-        if ()
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Make your move. || Choose from 1 to 3 on X and Y coordinates.");
+        try {
+            String myMove = br.readLine();
+            Byte myBestMove = Byte.parseByte(myMove);
+            if (myBestMove <= 0 || myBestMove >= 3 ) {
+            }
+        }
+        catch ()
 
+    }
+
+    @Override
+    public boolean makeAMove() {
+        return false;
     }
 
     @Override

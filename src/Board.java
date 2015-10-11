f25fbc55b883d30db721a37a23f003eac25ccb9a
IlyaKurmaz <ilya.kurmaz@gmail.com>
2015-10-11 11:56:13 +0300

    SnovaVDeleDoska

--- src/Board.java
+++ src/Board.java
@@ -1,24 +1,21 @@
 /**
  * Created by admin on 11.10.2015.
  */
-public class Board
-{
+public class Board {
     public static char[][] Field =
-            {       { '+', '+', '+' },
-                    { '+', '+', '+' },
-                    { '+', '+', '+' } };
+            {{'+', '+', '+'},
+                    {'+', '+', '+'},
+                    {'+', '+', '+'}};
 
-    public static void PrintField()
-    {
+    public static void PrintField() {
         int i;
         for (i = 0; i < 3; i++) {
-
-        }
-        int j;
+            int j;
             for (j = 0; j < 3; j++) {
                 System.out.print(Field[i][j]);
             }
             System.out.println();
-    }
+        }
 
+    }
 }

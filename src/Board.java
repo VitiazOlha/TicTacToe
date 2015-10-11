7153208b5358a70a0cb6047aa625a2a75fc454e3
IlyaKurmaz <ilya.kurmaz@gmail.com>
2015-10-11 16:30:42 +0300

    Test

--- src/Board.java
+++ src/Board.java
@@ -8,10 +8,10 @@ public class Board {
     public static void printBoard() {
         int i = 0;
         for (; i < 2; i++) {
-            System.out.printf(" %c ║ %c ║ %c \n",makeSymbol(field[i][0]),makeSymbol(field[i][1]),makeSymbol(field[i][2]));
-            System.out.println("═══╬═══╬═══");
+            System.out.printf(" %c ? %c ? %c \n",makeSymbol(field[i][0]),makeSymbol(field[i][1]),makeSymbol(field[i][2]));
+            System.out.println("???????????");
         }
-        System.out.printf(" %c ║ %c ║ %c \n",makeSymbol(field[i][0]),makeSymbol(field[i][1]),makeSymbol(field[i][2]));
+        System.out.printf(" %c ? %c ? %c \n",makeSymbol(field[i][0]),makeSymbol(field[i][1]),makeSymbol(field[i][2]));
     }
 
     public static char makeSymbol(byte value){
@@ -39,6 +39,25 @@ public class Board {
             field[x - 1][y - 1] = value;
             return true;
         }
+    }
+    public static byte checkGame() {
+        byte winner = 0;
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0] == field[i][1] && field[i][1] == field[i][2]) {
+                winner = field[i][0];
+            } else if (field[0][i] == field[1][i] && field[1][i] == field[2][i]) {
+                winner = field[0][i];
+            }
+        }
+        if (winner == 0) {
+            if (field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
+                winner = field[0][0];
+            } else if (field[0][2] == field[1][1] && field[1][1] == field[0][2]) {
+                winner = field[0][2];
+            }
+
+        }
+        return winner;
     }
 }
 

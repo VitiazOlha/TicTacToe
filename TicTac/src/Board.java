c226f0fab1729f06d1e8bdea6b5ee6a701d18e9e
IlyaKurmaz <ilya.kurmaz@gmail.com>
2015-10-11 11:52:24 +0300

    SnovaVDeleDoska

--- TicTac/src/Board.java
+++ TicTac/src/Board.java
@@ -3,5 +3,22 @@
  */
 public class Board
 {
+    public static char[][] Field =
+            {       { '+', '+', '+' },
+                    { '+', '+', '+' },
+                    { '+', '+', '+' } };
+
+    public static void PrintField()
+    {
+        int i;
+        for (i = 0; i < 3; i++) {
+
+        }
+        int j;
+            for (j = 0; j < 3; j++) {
+                System.out.print(Field[i][j]);
+            }
+            System.out.println();
+    }
 
 }

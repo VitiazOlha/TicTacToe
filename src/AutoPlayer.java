7a15aef687405d0fdc70d2267d12fcf5b5d8407f
Maks <gumanyuk.maks@gmail.com>
2015-10-11 21:40:05 +0300

    Merge branch 'master' of C:\Users\Maks\IdeaProjects\TicTacToe with conflicts.

--- src/AutoPlayer.java
+++ src/AutoPlayer.java
@@ -1,17 +1,16 @@
 /**
  * Created by admin on 11.10.2015.
  */
-public class AutoPlayer //extends DefaultPlayer
+public class AutoPlayer extends DefaultPlayer
 {
-/*public static void ComputerMove()
+public static void ComputerMove()
 {
     int x = (int) (Math.random() * 3), y = (int) (Math.random() * 3);
-    while (Board[x][y] == -1|| Board[x][y] == 1) {
+    while (Board[x][y] == -1 || Board[x][y] == 1) {
         x = (int) (Math.random() * 3);
         y = (int) (Math.random() * 3);
     }
     Board[x][y] = '0';
-    //loll
 
 }
 
@@ -23,5 +22,13 @@ public class AutoPlayer //extends DefaultPlayer
     @Override
     public boolean canMove() {
         return false;
-    }*/
+    }
+
+    public static char[][] algoritm(char [][] bordArray) {
+        if ( bordArray[2][2] == 'X' && RAUND = 1 ) { bordArray[1][1] = 'O'; RAUND++}
+            else if (bordArray[2][2] == '' && RAUND = 1) { bordArray[2][2] = 'O'; RAUND++}
+        }
+
+        }
+    }
 }

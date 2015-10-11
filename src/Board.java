c77140ff766803f7636a92c7066b52051c0f1374
Olha Vitiaz <vitiazolha@gmail.com>
2015-10-11 14:20:29 +0300

    GameClass

--- src/Board.java
+++ src/Board.java
@@ -2,19 +2,25 @@
  * Created by admin on 11.10.2015.
  */
 public class Board {
-    public static byte[][] Field =
+    public static byte[][] field =
             {       {0, 0, 0},
                     {0, 0, 0},
                     {0, 0, 0}
             };
 
-    public static void PrintField() {
+    //todo rewrite like toString(formating)
+    public void printBoard() {
         for ( int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(Field[i][j]);
+                System.out.print(field[i][j]);
             }
+        }
             System.out.println();
         }
+    }
 
+    public void getCooedinates(byte x, byte y, byte value) {
+        field[x][y] = value;
     }
 }
+

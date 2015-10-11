d2d848bee00e98c021e5c6f03547186814a1081d
Olha Vitiaz <vitiazolha@gmail.com>
2015-10-11 12:14:26 +0300

    GameClass updated

--- src/GameClass.java
+++ src/GameClass.java
@@ -3,5 +3,22 @@
  */
 public class GameClass
 {
+    public static void main(String[] args) {
+        byte N = 2;
+        DefaultPlayer [] players = new DefaultPlayer[N];
+        players[0] = new HumanPlayer();
+        players[1] = new AutoPlayer();
+        startNewGame(players);
 
+
+    }
+
+
+    public static void startNewGame(DefaultPlayer[] players){
+        boolean flag = true;
+        for (int i = 0; (i < 9) && flag; i++) {
+            flag = players[i % 2].makeAMove();
+        }
+
+    }
 }

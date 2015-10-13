2257cc792f90f04bd00cb907f126344435af34fd
Olha Vitiaz <vitiazolha@gmail.com>
2015-10-13 11:56:17 +0300

    Make a move is writing

--- src/AutoPlayer.java
+++ src/AutoPlayer.java
@@ -1,31 +1,74 @@
-/**
- * Created by admin on 11.10.2015.
- */
-public class AutoPlayer extends Player {//todo automatic algortm Olga
+public class AutoPlayer extends Player {
+    final static byte N = 3;
+    private byte[][] field = new byte[N][N];
+
+    private void copyBoard(byte value) {
+        for (byte x = 0; x < 3; x++) {
+            for (byte y = 0; y < 3; y++) {
+                field[x][y] = (byte) (value * Board.getCoordinates(x, y));
+            }
+        }
+    }
 
     @Override
     public void makeAMove(byte value) {
+        copyBoard(value);
+        byte bestX = -1, bestY = -1;
+        for (byte x = 0; x < N; x++) {
+            for (byte y = 0; y < N; y++) {
+                byte max = -5;
+                if (field[x][y] == 0) {
+                    byte newMax = maxMove();
+                    if (newMax > max){
+                        max = newMax;
+                        bestX = x;
+                        bestY = y;
+                    }
+                }
+            }
+        }
+        Board.setCoordinates(bestX, bestY, value);
+    }
 
+    private byte maxMove() {
+
+
+        return 0;
     }
-/*public static void ComputerMove()
-{
-    int x = (int) (Math.random() * 3), y = (int) (Math.random() * 3);
-    while (Board[x][y] == -1|| Board[x][y] == 1) {
-        x = (int) (Math.random() * 3);
-        y = (int) (Math.random() * 3);
+
+    private byte minMove() {
+
+
+        return 0;
     }
-    Board[x][y] = '0';
-    //loll
+
+    private byte checkRow(byte value) {//ряд
+        for (int i = 0; i < N; i++) {
+
+        }
+        return 0;
+    }
+
+    private byte checkColumn(byte value) {//столбец
+        for (int i = 0; i < N; i++) {
+
+        }
+        return 0;
+    }
+
+    private byte checkMainDiagonal() {
+
+        return 0;
+    }
+
+    private byte checkSideDiagonal() {
+
+        return 0;
+    }
+
+
+
+
 
 }
 
-    @Override
-    public boolean makeAMove() {
-        return false;
-    }
-
-    @Override
-    public boolean canMove() {
-        return false;
-    }*/
-}

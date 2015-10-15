32f402946097c441ce257d52aa809f4e49145b99
IlyaKurmaz <ilya.kurmaz@gmail.com>
2015-10-15 18:21:58 +0300

    Test

---
+++ src/CheckWinnerTest.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by admin on 13.10.2015.
+ */
+public class CheckWinnerTest {
+
+    @Test(timeout = 100)
+    public void testCheckWinner() throws Exception {
+
+        int[][] board = {{1, -1, 1},
+                         {1, -1, 1},
+                         {1, 0, 0}}; //Создаем заполненную доску с одним или двумя пустыми ячеками (0)
+        Assert.assertEquals(1, CheckTheValue.checkCell(1, 0, board));
+
+        int[][] board1 ={{-1, 1, 1},
+                {-1, 1, -1},
+                {-1, -1, 0}}; //Создаем заполненную доску с одним или двумя пустыми ячеками (0)
+        Assert.assertEquals(-1, CheckTheValue.checkCell(1, 0, board1));
+
+
+        int[][] board2 = {{-1, -1, 1},
+                {1, 1, -1},
+                {-1, 1, 0}}; //Создаем заполненную доску с одним или двумя пустыми ячеками (0)
+        Assert.assertEquals(0, CheckTheValue.checkCell(1, 0, board2));
+
+    }
+
+
+
+}
---
+++ src/MakeSymbolTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+import sun.invoke.empty.Empty;
+
+import static org.junit.Assert.*;
+
+/*
+ * Created by admin on 15.10.2015.
+ */
+public class MakeSymbolTest {
+
+    @Test(timeout = 100)
+    public void testMakeSymbol() throws Exception {
+
+        int value = -1;
+        Assert.assertEquals('O',Board.makeSymbol(value));
+
+        int value1 = 1;
+        Assert.assertEquals('X', Board.makeSymbol(value1));
+
+       int value2 = 5;
+        Assert.assertEquals(' ', Board.makeSymbol(value2));
+
+        int value3 = -99;
+        Assert.assertEquals(' ', Board.makeSymbol(value3));
+
+       int value4 = Integer.parseInt(null);
+        Assert.assertEquals(' ', Board.makeSymbol(value4));
+
+        int value5 = (int) -99.6;
+        Assert.assertEquals(' ', Board.makeSymbol(value5));
+
+
+
+
+
+    }
+}
